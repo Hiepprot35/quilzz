809274a6a9910acd0e87be98dca8ee99a9658960
Tuan Hiep <tansou57@gmail.com>
2023-10-25 02:47:55 +0700

    oke

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import AddDocument from './component/addDocument/AddDocument';
 import ViewTerm from './component/viewTerm/viewTerm';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
+
       <Routes>
         <Route path="*" element={<AddDocument />} />
         <Route path="/view" element={<ViewTerm />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
+
   );
 }
 
